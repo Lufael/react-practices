d0a70a3464917dd2a2004afc354f0c32f0048953
Suthipong C.vidhayakul <lufael.sc@gmail.com>
2023-08-03 18:34:42 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
+// 1) Import the React and ReactDOM libraries
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// 2) Get a reference to the div with ID root 
+const el = document.getElementById("root");
+
+// 3) Get a reference to the div with ID root
+const root = ReactDOM.createRoot(el);
+
+// 4) Create a new component
+ function App() {
+    
+    return <textarea autoFocus={true} />;
+ }
+
+ // 5) Show the component on the screen
+ root.render(<App />);
